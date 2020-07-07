f9e0951a0bd79338c682458ad9870d73e8127136
Luca Alexandru <luca.p.alexandru@gmail.com>
2020-07-07 23:54:48 +0300

    first commit

---
+++ src/testcase.js
@@ -0,0 +1,142 @@
+// Licensed to the Software Freedom Conservancy (SFC) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The SFC licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+import CommandEmitter from './command'
+import config from './config'
+import { stringEscape } from '@seleniumhq/side-utils'
+
+const hooks = []
+
+export function emit(test, options = config, snapshot) {
+  return new Promise(async (res, rej) => {
+    // eslint-disable-line no-unused-vars
+    const hookResults = await Promise.all(hooks.map(hook => hook(test)))
+    const setupHooks = hookResults
+      .map(hook => hook.setup || '')
+      .filter(hook => !!hook)
+    const teardownHooks = hookResults
+      .map(hook => hook.teardown || '')
+      .filter(hook => !!hook)
+
+    let errors = []
+
+    const commands = await Promise.all(
+      test.commands.map((command, index) =>
+        CommandEmitter.emit(
+          command,
+          options,
+          snapshot ? snapshot.commands[command.id] : undefined
+        ).catch(e => {
+          if (options.silenceErrors) {
+            return `throw new Error("${e.message}");`
+          } else {
+            errors.push({
+              index: index + 1,
+              ...command,
+              message: e,
+            })
+          }
+        })
+      )
+    )
+
+    if (errors.length) {
+      rej({ ...test, commands: errors })
+    }
+
+    if (!options.skipStdLibEmitting) {
+      // emit everything
+      const testName = stringEscape(test.name)
+      let emittedTest = `it("${testName}", async () => {`
+      emittedTest += setupHooks
+        .join('')
+        .concat(snapshot ? snapshot.setupHooks.join('') : '')
+      emittedTest += `await tests["${testName}"](driver, vars);`
+      emittedTest += 'expect(true).toBeTruthy();'
+      emittedTest += teardownHooks
+        .join('')
+        .concat(snapshot ? snapshot.teardownHooks.join('') : '')
+      emittedTest += '});'
+
+      let _commands = [
+        'let times = []; let timeStart = new Date().getTime(); let timeNow; let took;',
+      ]
+
+      commands.forEach((c, idx) => {
+        let originalCommand = test.commands[idx]
+        let commandName = `${originalCommand.command} on ${
+          originalCommand.target
+        }`
+        if (originalCommand.value) {
+          commandName += ` with value ${originalCommand.value}`
+        }
+        _commands.push(c)
+        _commands.push(
+          `timeNow = new Date().getTime(); took = timeNow - timeStart; times.push({name: '${commandName}', took}); timeStart = timeNow;`
+        )
+      })
+
+      _commands.push(
+        `require('fs').writeFileSync(require('path').join(process.cwd(), '..', '${testName}_execution_time.json'), JSON.stringify(times, null, 4));`
+      )
+
+      let func = `tests["${testName}"] = async (driver, vars, opts = {}) => {`
+      func += _commands.join('')
+      func += '}'
+
+      res({ id: test.id, name: testName, test: emittedTest, function: func })
+    } else {
+      // emit only additional hooks
+      let snapshot = {
+        commands: commands.reduce((snapshot, emittedCommand, index) => {
+          if (!emittedCommand.skipped) {
+            snapshot[test.commands[index].id] = emittedCommand
+          }
+          return snapshot
+        }, {}),
+        setupHooks,
+        teardownHooks,
+      }
+
+      if (
+        Object.keys(snapshot.commands).length ||
+        snapshot.setupHooks.length ||
+        snapshot.teardownHooks.length
+      ) {
+        // if we even snapshotted anything
+        res({ id: test.id, snapshot })
+      } else {
+        // resolve to nothing if there is no snapshot
+        res({})
+      }
+    }
+  })
+}
+
+export function registerHook(hook) {
+  hooks.push(hook)
+}
+
+export function clearHooks() {
+  hooks.length = 0
+}
+
+export default {
+  emit,
+  registerHook,
+  clearHooks,
+}
